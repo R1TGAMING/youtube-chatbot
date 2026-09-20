74ff6db5e9aa9453727cce4527354dd8ee67dfae
ipii <rafisofyangaming1234@gmail.com>
2026-09-20 08:11:09 +0700

    edit: supabase.ts

--- frontend/src/utils/supabase.ts
+++ frontend/src/utils/supabase.ts
@@ -1,6 +1,15 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
+if (
+  !import.meta.env.VITE_SUPABASE_URL ||
+  !import.meta.env.VITE_SUPABASE_ANON_KEY
+) {
+  console.log(
+    "Please set VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY inn .env files",
+  );
+}
+
 export const supabase: SupabaseClient = createClient(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!,
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
